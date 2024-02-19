163b2fd3f214bc015059072c387d7e54fe39cd1d
ziinup <wlsgmlxo@gmail.com>
2024-02-19 11:41:43 +0900

    윈도우 GUI 활용으로 변경 	modified:   imgcrop.py

--- imgcrop.py
+++ imgcrop.py
@@ -1,5 +1,9 @@
 import os
 from PIL import Image
+from tkinter import Tk, messagebox
+from tkinter.filedialog import askdirectory
+import glob
+
 
 def resize_and_crop(image_path, output_path, target_size=(1000, 1310), background_color=(0, 0, 0)):
     with Image.open(image_path) as img:
@@ -24,18 +28,33 @@ def resize_and_crop(image_path, output_path, target_size=(1000, 1310), backgroun
         # 결과 이미지 저장
         img_cropped.save(output_path, quality=95)
 
-# 예제 사용
-image_directory = 'D:\\작업\\240216_PLZPROJECT 룩북 등록\\plz24ss'
-output_directory = image_directory + '\\output'
-import glob
+def main():
+ # Tkinter root 생성
+    root = Tk()
+    root.withdraw()  # Tkinter 창을 숨깁니다.
 
-# 해당 디렉토리의 모든 JPG 파일에 대해 처리
-for image_path in glob.glob(image_directory + '\\*.jpg'):
-    # 출력 파일 경로 생성
-    filename = os.path.basename(image_path)
-    output_path = os.path.join(output_directory, filename)
+    # 폴더 선택 대화상자 표시
+    image_directory = askdirectory(title='Select Image Directory')
+    if not image_directory:
+        messagebox.showinfo("Information", "No directory selected, exiting...")
+        return
+
+    output_directory = os.path.join(image_directory, "output")
+    os.makedirs(output_directory, exist_ok=True)
+
+    processed_files = 0
+    # 해당 디렉토리의 모든 JPG 파일에 대해 처리
+    for image_path in glob.glob(os.path.join(image_directory, '*.jpg')):
+        # 출력 파일 경로 생성
+        filename = os.path.basename(image_path)
+        output_path = os.path.join(output_directory, filename)
+        
+        # 리사이즈 및 필요에 따라 이미지 크롭 함수 호출
+        resize_and_crop(image_path, output_path)
+        processed_files += 1
+
+    messagebox.showinfo("Complete", f"All images processed. Total: {processed_files} files.")
+    root.destroy()
     
-    # 리사이즈 및 필요에 따라 이미지 크롭 함수 호출
-    resize_and_crop(image_path, output_path)
-
-print("모든 이미지 처리가 완료되었습니다.")
+if __name__ == "__main__":
+    main()
